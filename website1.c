e35e78bb77810e8e32b66b38e03e4b880eb23c92
d1gt774 <xandergonder@outlook.com>
2024-12-09 17:28:12 -0700

    Create website1.c

---
+++ website1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Content-type: text/html\n\n");
+    printf("<html><body><h1>Hello, this is a C-generated webpage!</h1></body></html>");
+    return 0;
+}
